963c76b88168cdf5a07c3727e1b138bac210a4d7
mkulik05 <mishakulik2@gmail.com>
2024-01-21 21:20:36 +0300

    4th

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use serde_json::{self, Value};
+use serde_json::{self, Value, Map};
 use std::env;
 fn decode_bencoded_value(mut encoded_value: &str) -> (Value, usize) {
-    // If encoded_value starts with a digit, it's a number
     match encoded_value.chars().next().unwrap() {
         x if x.is_ascii_digit() => {
             let colon_index = encoded_value.find(':').unwrap();
@@ -9,28 +8,45 @@ fn decode_bencoded_value(mut encoded_value: &str) -> (Value, usize) {
             let number = number_string.parse::<i64>().unwrap();
             let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
             return (string.into(), colon_index + 1 + number as usize);
-        },
+        }
         'l' => {
             let mut list = Vec::new();
             let mut total_len = 2; // counting symbols 'l' and 'e'
             encoded_value = encoded_value.strip_prefix('l').unwrap();
             loop {
-                if encoded_value.is_empty() {break}
-                if encoded_value.starts_with('e') {break}
+                if encoded_value.is_empty() || encoded_value.starts_with('e') {
+                    break;
+                }
                 let (list_part, len) = decode_bencoded_value(encoded_value);
                 total_len += len;
                 list.push(list_part.clone());
                 encoded_value = &encoded_value[len..];
             }
-            return(Value::Array(list), total_len);
-                        
-        },
+            return (Value::Array(list), total_len);
+        }
         'i' => {
             let num_end = encoded_value.find('e').unwrap();
             let int_res = encoded_value[1..num_end].parse::<i64>();
             if let Ok(n) = int_res {
-                return (n.into(), num_end + 1)
-            }   
+                return (n.into(), num_end + 1);
+            }
+        },
+        'd' => {
+            let mut dict = Map::new();
+            encoded_value = &encoded_value[1..];
+            let mut total_len = 2;
+            loop {
+                if encoded_value.is_empty() || encoded_value.starts_with('e') {
+                    println!("fnjd");
+                    break;
+                }
+                let (key, key_len) = decode_bencoded_value(encoded_value);
+                let (value, val_len) = decode_bencoded_value(&encoded_value[key_len..]);
+                total_len += val_len + key_len;
+                dict.insert(key.to_string(), value);
+                encoded_value = &encoded_value[val_len + key_len..];
+            }
+            return (dict.into(), total_len);
         },
         _ => {}
     }
